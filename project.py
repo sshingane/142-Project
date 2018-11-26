8abc289b4c87f0710392e4a364e8ce8193e65720
sshingane <sshingan@ucsc.edu>
2018-11-25 22:47:37 -0800

    some sort of tf-idf

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 import numpy as np
 import re
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 import collections
 
 def tokenize_sentences(sentences):
@@ -19,7 +20,8 @@ def extract_words(sentence):
     return words_cleaned    
     
 def bagofwords(sentence, words):
-    sentence_words = extract_words(sentence)
+    for i in sentence: 
+        sentence_words[i] = extract_words(sentence[i])
     # frequency word count
     bag = np.zeros(len(words))
     for sw in sentence_words:
@@ -30,22 +32,42 @@ def bagofwords(sentence, words):
     return np.array(bag)
 
 sentences = []
+size = 0
 stop = ['a', 'the', 'of', 'and', 'to', 'is']
 with open('train.csv') as my_file:
     for line in my_file:
+        size += 1
         sentences.append(line)
 
-vocabulary = tokenize_sentences(sentences)
-bagofwords("the only thing Avary seems to care about are mean giggles and pulchritude", vocabulary)
+#vocabulary = tokenize_sentences(sentences)
+#bagofwords("the only thing Avary seems to care about are mean giggles and pulchritude", vocabulary)
 #bags = [ collections.Counter(re.findall(r'\w+', txt)) for txt in sentences]
 #print "got bags"
 #sumbags = sum(bags, collections.Counter())
 #print "got sumbags"
 
-vectorizer = CountVectorizer(analyzer = "word", tokenizer = None, preprocessor = None, stop_words = stop, max_features = 5000) 
-train_data_features = vectorizer.fit_transform(sentences)
+vectorizer = TfidfVectorizer(analyzer = "word", tokenizer = None, preprocessor = None, stop_words = stop, max_features = 5000) 
+vectorizer.fit(sentences)
 vectorizer.transform(sentences).toarray()
+vector = vectorizer.transform(sentences)
+vector.todok().keys()
+vector.todok().items()
+#v_array = vector.toarray()
 
-print vectorizer.vocabulary_
+#print vectorizer.vocabulary_
+#print vectorizer.idf_\
+'''
+print vector.shape
+print len(vector.toarray())
+print 'data:'  
+print v_array.data
+print 'row:' 
+print v_array.row
+print 'col:' 
+print v_array.col
+print size
+'''
+
+print vector
 #print '\n'.join(str(line) for line in vocabulary) 
 #print sumbags
