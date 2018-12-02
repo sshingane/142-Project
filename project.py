1dfa4d5b69fcc5468d807522bf8a8ad9804d30f4
rishitaro <riroy@ucsc.edu>
2018-12-01 18:13:45 -0800

    Add SVM, Perceptron, cross validation

--- project.py
+++ project.py
@@ -11,6 +11,9 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import confusion_matrix
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import Perceptron
+from sklearn.model_selection import cross_validate
 
 
 """
@@ -26,32 +29,6 @@ def open_file():
         labels.append(int(line[3]))
     return (instances, labels)
 
-# def tokenize_sentences(sentences):
-#     words = []
-#     for sentence in sentences:
-#         w = extract_words(sentence)
-#         words.extend(w)        
-#     words = sorted(list(set(words)))
-#     return words
-
-# def extract_words(sentence):
-#     ignore_words = ['a', 'the', 'of', 'and', 'to', 'is']
-#     words = re.sub("[^\w]", " ",  sentence).split() #nltk.word_tokenize(sentence)
-#     words_cleaned = [w.lower() for w in words if w not in ignore_words]
-#     return words_cleaned    
-    
-# def bagofwords(sentence, words):
-#     for i in sentence: 
-#         sentence_words[i] = extract_words(sentence[i])
-#     # frequency word count
-#     bag = np.zeros(len(words))
-#     for sw in sentence_words:
-#         for i,word in enumerate(words):
-#             if word == sw: 
-#                 bag[i] += 1
-                
-#     return np.array(bag)
-
 def trim(data):
     v_trim = []
     for line in data: 
@@ -59,12 +36,6 @@ def trim(data):
             v_trim.append(line)
     return v_trim  
 
-# def label_parse(sentence):    
-#     labels = []
-#     for i in sentences: 
-#         labels.append(i[3])
-
-#     return labels
 def count_vectorize(instances):
     vectorizer = CountVectorizer(analyzer = 'word', stop_words = stopwords.words('english'))
     corpus = []
@@ -105,27 +76,51 @@ def naive_bayes(feature_vector_matrix, actual_labels):
     nb.fit(feature_vector_matrix, actual_labels)
     predicted_labels = nb.predict(feature_vector_matrix)
     return predicted_labels
+
+def svm(feature_vector_matrix, actual_labels):
+    svm = LinearSVC(random_state=0, tol=1e-5, max_iter=4000)
+    svm_fit = svm.fit(feature_vector_matrix, actual_labels)
+    prediction = svm_fit.predict(feature_vector_matrix)
+    return prediction
+
+def perceptron(feature_vector_matrix, actual_labels):
+    percep = Perceptron(tol=1e-3, random_state=0)
+    percep.fit(feature_vector_matrix, actual_labels)
+    prediction = percep.predict(feature_vector_matrix)
+
+    return prediction
     
 def printPerformance(model_name, actual_labels, predicted_labels):
     labels = [0, 1, 2, 3, 4]
     conf_matrix = sklearn.metrics.confusion_matrix(actual_labels, predicted_labels, labels = labels)
     accuracy = sklearn.metrics.accuracy_score(actual_labels, predicted_labels)
     precision = sklearn.metrics.precision_score(actual_labels, predicted_labels, labels=labels, average=None)
+    recall = sklearn.metrics.recall_score(actual_labels, predicted_labels, labels=labels, average=None)
     print('='*60)
     print(model_name)
     print('='*60)
     print('CONFUSION MATRIX:')
     print(conf_matrix)
-    print('ACCURACY: ' + str(accuracy))
+    print('ACCURACY: ' + str(accuracy) + ' Ava')
+    print('RECALL: ' + str(recall))
     print('PRECISION: ' + str(precision) + '\n')
+    
+
+def cross_validation(instance, labels):
+    clf = MultinomialNB()
+    cv_result = cross_validate(
+        clf, instance, labels, cv=7, return_train_score=True)
+    test_score = cv_result.get('test_score')
+    train_score = cv_result.get('train_score')
+    print('5 folds, test score: ', test_score, "train score: ", train_score)
+
 
 if __name__ == '__main__':
     # Open file 
     file_data = open_file()
     instances = file_data[0]
     actual_labels = file_data[1]
-    # print(actual_labels)
-    # print(len(actual_labels))
+    
     
     # Number of training instances
     print(len(instances))    
@@ -149,21 +144,18 @@ if __name__ == '__main__':
     printPerformance('Naive Bayes + tfid', actual_labels, naive_bayes_predicted_labels)
     printPerformance('Naive Bayes + count', actual_labels, naive_bayes_predicted_labels_count)
 
-    # feature_vector.sort_indices()
-    # v_data = [] 
-    # v_data = vectorized_instances.data
-    # #v_data.sort()
-    # v_partial = []
-    # v_partial = trim(v_data)
-    # v_merge = []
-    # i = 0
-    # end = 0
+    # Call SVM classifier 
+    svm_predicted_labels = svm(tfid_vector_matrix, actual_labels)
+    svm_predicted_labels_count = svm(count_vector_matrix, actual_labels)
 
-    # while end != 109234:
-    #     if (v_partial[end]-(vectorized_instances[end].data)).any():
-    #         v_merge.append(vectorized_instances[end].indices)
-    #     end += 1
+    printPerformance('SVM + tfid', actual_labels, svm_predicted_labels)
+    printPerformance('SVM + count', actual_labels, svm_predicted_labels_count)
 
-    # with open('./output_6.txt', 'w+') as file_out:
-    #     for item in v_merge:
-    #         file_out.write("%s\n" % item)
+    # Call Perceptron classifier
+    percep_predicted_labels = perceptron(tfid_vector_matrix, actual_labels)
+    percep_predicted_labels_count = perceptron(count_vector_matrix, actual_labels)
+
+    printPerformance('Perceptron + tfid', actual_labels, percep_predicted_labels)
+    printPerformance('Perceptron + count', actual_labels, percep_predicted_labels_count)
+
+    cross_validation(tfid_vector_matrix, actual_labels)
